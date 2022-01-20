371daa380e6bd52df4c1f9e0b53a835e82af141d
flordagotto <68953960+flordagotto@users.noreply.github.com>
2022-01-20 17:26:25 +0100

    Add files via upload

---
+++ SoftphonesManager/SoftphonesManager/FilesManager/ColorsManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftphonesManager.FilesManager
+{
+    public class ColorsManager
+    {
+        private string ColorPrimary;
+        private string ColorAccent;
+        public ColorsManager(string primaryColor, string accentColor)
+        {
+            ColorPrimary = primaryColor;
+            ColorAccent = accentColor;
+        }
+
+        public void ModifyFile(string path)
+        {
+            try
+            {
+                var line = 0;
+                string[] lineas = File.ReadAllLines(path);
+
+                foreach (var linea in lineas)
+                {
+                    if (linea.Contains("colorAccent"))
+                        lineas[line] = "<color name=\"colorAccent\">" + ColorAccent + "</color>";
+                    else if (linea.Contains("colorPrimary"))
+                        lineas[line] = "<color name=\"colorPrimary\">" + ColorPrimary + "</color>";
+
+                    line++;
+                }
+                File.WriteAllLines(path, lineas);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ SoftphonesManager/SoftphonesManager/FilesManager/IconGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftphonesManager.FilesManager
+{
+    public class IconGenerator
+    {
+        public IconGenerator(string path)
+        {
+
+        }
+    }
+}
